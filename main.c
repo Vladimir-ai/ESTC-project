9e05a39ade1c08779e8353a73614e5cd078bed4b
krevedko <bespalvovan42@gmail.com>
2021-11-05 19:38:40 +0300

    Replaced bsp led to hal_gpio leds

--- main.c
+++ main.c
@@ -5,15 +5,29 @@
 
 #define MAX_BLINKS              7
 
+__STATIC_INLINE void init_leds(void);
+
+__STATIC_INLINE void init_leds(void)
+{
+    const uint32_t leds_array[] = LEDS_LIST;
+    uint8_t idx = 0;
+
+    for(idx = 0; idx < LEDS_NUMBER; idx++)
+    {
+        nrf_gpio_cfg_output(leds_array[idx]);
+        nrf_gpio_pin_write(leds_array[idx], 1);
+    }
+}
+
 /**
  * @brief Function for application main entry.
  */
 int main(void)
 {
     const uint8_t inv_num[] = {6, 5, 7, 7};
+    const uint32_t leds_array[] = LEDS_LIST;
 
-    /* Configure board. */
-    bsp_board_init(BSP_INIT_LEDS);
+    init_leds();
 
     /* Toggle LEDs. */
     while (true)
@@ -23,10 +37,10 @@ int main(void)
             for (uint8_t blink_num = 0; blink_num < MAX_BLINKS; blink_num++)
             {
                 if (blink_num < inv_num[i]){
-                    bsp_board_led_on(i);
+                    nrf_gpio_pin_toggle(leds_array[i]);
                     nrf_delay_ms(100);
 
-                    bsp_board_led_off(i);
+                    nrf_gpio_pin_toggle(leds_array[i]);
                     nrf_delay_ms(100);
                 }
             }
