061f47dd0e81411b5724484631f02d46c1f239e6
krevedko <bespalvovan42@gmail.com>
2021-11-14 20:11:10 +0300

    Added timer and irq events. Closed #12.

--- main.c
+++ main.c
@@ -13,12 +13,58 @@
 #include "app_usbd_serial_num.h"
 
 #include "pwm_module.h"
+#include "nrfx_gpiote.h"
+#include "app_timer.h"
 
 #define DUTY_CICLE_UNIT_TIME_US         10U
 #define FULL_PERIOD                     200U
 
+/* Timer timeouts */
+#define BTN_DISABLE_ACTIVITY_TIMEOUT          (APP_TIMER_CLOCK_FREQ >> 4) /* part of sec */
+#define BTN_DOUBLE_CLICK_TIMEOUT              APP_TIMER_CLOCK_FREQ        /* 1 sec timeout */
+
+/* Application flags */
+#define APP_FLAG_IS_RUNNING_MASK                 0x01U
+#define APP_FLAG_FST_CLICK_OCCURRED_MASK         0x02U
+
+/* static vars declaration */
+static uint32_t timer_start_timestamp;
+static uint8_t app_flags = 0;
+APP_TIMER_DEF(timeout_timer_id);
+
 /* static function declaration  */
-static void init_all(void);
+static void init_all(const nrfx_gpiote_in_config_t *btn_gpiote_cfg);
+
+static void timer_timeout_handler(void *p_context)
+{
+    /* Clear fst click if it isn't occured */
+    app_flags &= ~APP_FLAG_FST_CLICK_OCCURRED_MASK;
+}
+
+static void btn_pressed_evt_handler(nrfx_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
+{
+    switch (action)
+    {
+    case NRF_GPIOTE_POLARITY_HITOLO:
+        if (!(app_flags & APP_FLAG_FST_CLICK_OCCURRED_MASK))
+        {
+            app_flags |= APP_FLAG_FST_CLICK_OCCURRED_MASK;
+            timer_start_timestamp = app_timer_cnt_get();
+
+            app_timer_start(timeout_timer_id, BTN_DOUBLE_CLICK_TIMEOUT, NULL);
+        }
+        /* timeout used to disable undefined button behavior after press */
+        else if ((app_timer_cnt_get() - timer_start_timestamp) > BTN_DISABLE_ACTIVITY_TIMEOUT)
+        {
+            app_flags ^= APP_FLAG_IS_RUNNING_MASK;
+            app_flags &= ~APP_FLAG_FST_CLICK_OCCURRED_MASK;
+        }
+        break;
+
+    default:
+        break;
+    }
+}
 
 static void logs_init()
 {
@@ -33,48 +79,57 @@ static void logs_init()
  */
 int main(void)
 {
+    const nrfx_gpiote_in_config_t btn_gpiote_cfg = {
+        .sense = NRF_GPIOTE_POLARITY_HITOLO,
+        .pull = NRF_GPIO_PIN_PULLUP,
+        .is_watcher = false,
+        .hi_accuracy = false,
+        .skip_gpio_setup = true
+    };
+
     const uint8_t inv_num[] = {6, 5, 7, 7};
     const char led_color[] = {'G', 'R', 'G', 'B'};
 
     uint8_t led_idx = 0;
     uint8_t blink_num = 0;
-    uint8_t current_cycle = 0;      /* Uses 7th bit to store state (increasing or decreasing), 0-6 bits to store percentage */
+    uint8_t current_cycle = 0;
 
-    init_all();
+    init_all(&btn_gpiote_cfg);
 
     /* Toggle LEDs. */
     while (true)
     {
-        if (!read_btn_state())
+        if (blink_num == inv_num[led_idx])
         {
-            if (blink_num == inv_num[led_idx])
-            {
-                blink_num = 0;
-                led_idx = (led_idx + 1) % LEDS_NUMBER;
+            blink_num = 0;
+            led_idx = (led_idx + 1) % LEDS_NUMBER;
 
-                continue;
-            }
+            continue;
+        }
 
-            pwm_process_one_period(led_idx, current_cycle > 100 ? 200U - current_cycle : current_cycle);
-            current_cycle++;
+        pwm_process_one_period(led_idx, current_cycle > 100 ? 200U - current_cycle : current_cycle);
 
-            switch (current_cycle)
-            {
-            case 101:
-                NRF_LOG_INFO("100%% duty cycle on %c LED, curr LED iter is %d/%d", led_color[led_idx], blink_num + 1, inv_num[led_idx]);
-                NRF_LOG_PROCESS();
-                break;
+        if (app_flags & APP_FLAG_IS_RUNNING_MASK)
+        {
+            current_cycle++;    /* Change cycle if not frozen */
+        }
 
-            case 201:
-                NRF_LOG_INFO("Btn cycle ended on %c LED, curr LED iter is %d/%d", led_color[led_idx], blink_num + 1, inv_num[led_idx]);
-                NRF_LOG_PROCESS();
-                current_cycle = 0;
-                blink_num++;
-                break;
+        switch (current_cycle)
+        {
+        case 101:
+            NRF_LOG_INFO("100%% duty cycle on %c LED, curr LED iter is %d/%d", led_color[led_idx], blink_num + 1, inv_num[led_idx]);
+            NRF_LOG_PROCESS();
+            break;
 
-            default:
-                break;
-            }
+        case 201:       /* Cycle overflow */
+            NRF_LOG_INFO("Btn cycle ended on %c LED, curr LED iter is %d/%d", led_color[led_idx], blink_num + 1, inv_num[led_idx]);
+            NRF_LOG_PROCESS();
+            current_cycle = 0;
+            blink_num++;
+            break;
+
+        default:
+            break;
         }
 
         LOG_BACKEND_USB_PROCESS();  /* Process here to maintain connect */
@@ -82,7 +137,7 @@ int main(void)
     }
 }
 
-static void init_all(void)
+static void init_all(const nrfx_gpiote_in_config_t *btn_gpiote_cfg)
 {
     /* Init systick */
     nrfx_systick_init();
@@ -90,9 +145,23 @@ static void init_all(void)
     /* Init logs */
     logs_init();
     NRF_LOG_INFO("Starting up the test project with USB logging");
-    NRF_LOG_PROCESS();
+    NRF_LOG_FLUSH();
 
     /* Init leds and btns */
     init_leds();
     init_btns();
+
+    /* Init gpiote */
+    APP_ERROR_CHECK(nrfx_gpiote_init());
+    APP_ERROR_CHECK(nrfx_gpiote_in_init(BUTTON_1, btn_gpiote_cfg, &btn_pressed_evt_handler));
+    NRF_LOG_INFO("GPIOTE initiated");
+    NRF_LOG_FLUSH();
+
+    nrfx_gpiote_in_event_enable(BUTTON_1, true);
+
+    APP_ERROR_CHECK(app_timer_init());
+    APP_ERROR_CHECK(app_timer_create(&timeout_timer_id, APP_TIMER_MODE_SINGLE_SHOT, &timer_timeout_handler));
+    NRF_LOG_INFO("App timer initiated");
+    NRF_LOG_FLUSH();
+
 }
