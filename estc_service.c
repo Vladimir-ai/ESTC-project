e04a2712a7142d405f6ab85ddc1004f0e40fc4f4
Vladimir Bespalov <bespalvovan42@gmail.com>
2022-04-17 13:57:59 +0300

    Set tabspace to 2 instead from 4

--- estc_service.c
+++ estc_service.c
@@ -41,44 +41,45 @@ static ret_code_t estc_ble_add_characteristics(ble_estc_service_t *service);
 
 ret_code_t estc_ble_service_init(ble_estc_service_t *service)
 {
-    ret_code_t error_code = NRF_SUCCESS;
-    // TODO: 4. Add service UUIDs to the BLE stack table using `sd_ble_uuid_vs_add`
-    // TODO: 5. Add service to the BLE stack using `sd_ble_gatts_service_add`
+  ret_code_t error_code = NRF_SUCCESS;
 
-    // NRF_LOG_DEBUG("%s:%d | Service UUID: 0x%04x", __FUNCTION__, __LINE__, service_uuid.uuid);
-    // NRF_LOG_DEBUG("%s:%d | Service UUID type: 0x%02x", __FUNCTION__, __LINE__, service_uuid.type);
-    // NRF_LOG_DEBUG("%s:%d | Service handle: 0x%04x", __FUNCTION__, __LINE__, service->service_handle);
+  // TODO: 4. Add service UUIDs to the BLE stack table using `sd_ble_uuid_vs_add`
+  // TODO: 5. Add service to the BLE stack using `sd_ble_gatts_service_add`
 
-    return estc_ble_add_characteristics(service);
+  // NRF_LOG_DEBUG("%s:%d | Service UUID: 0x%04x", __FUNCTION__, __LINE__, service_uuid.uuid);
+  // NRF_LOG_DEBUG("%s:%d | Service UUID type: 0x%02x", __FUNCTION__, __LINE__, service_uuid.type);
+  // NRF_LOG_DEBUG("%s:%d | Service handle: 0x%04x", __FUNCTION__, __LINE__, service->service_handle);
+
+  return estc_ble_add_characteristics(service);
 }
 
 static ret_code_t estc_ble_add_characteristics(ble_estc_service_t *service)
 {
-    ret_code_t error_code = NRF_SUCCESS;
+  ret_code_t error_code = NRF_SUCCESS;
 
-    // TODO: 6.1. Add custom characteristic UUID using `sd_ble_uuid_vs_add`, same as in step 4
-
-
-    // TODO: 6.5. Configure Characteristic metadata (enable read and write)
-    ble_gatts_char_md_t char_md = { 0 };
+  // TODO: 6.1. Add custom characteristic UUID using `sd_ble_uuid_vs_add`, same as in step 4
 
 
-    // Configures attribute metadata. For now we only specify that the attribute will be stored in the softdevice
-    ble_gatts_attr_md_t attr_md = { 0 };
-    attr_md.vloc = BLE_GATTS_VLOC_STACK;
+  // TODO: 6.5. Configure Characteristic metadata (enable read and write)
+  ble_gatts_char_md_t char_md = { 0 };
 
 
-    // TODO: 6.6. Set read/write security levels to our attribute metadata using `BLE_GAP_CONN_SEC_MODE_SET_OPEN`
+  // Configures attribute metadata. For now we only specify that the attribute will be stored in the softdevice
+  ble_gatts_attr_md_t attr_md = { 0 };
+  attr_md.vloc = BLE_GATTS_VLOC_STACK;
 
 
-    // TODO: 6.2. Configure the characteristic value attribute (set the UUID and metadata)
-    ble_gatts_attr_t attr_char_value = { 0 };
+  // TODO: 6.6. Set read/write security levels to our attribute metadata using `BLE_GAP_CONN_SEC_MODE_SET_OPEN`
 
 
-    // TODO: 6.7. Set characteristic length in number of bytes in attr_char_value structure
+  // TODO: 6.2. Configure the characteristic value attribute (set the UUID and metadata)
+  ble_gatts_attr_t attr_char_value = { 0 };
 
 
-    // TODO: 6.4. Add new characteristic to the service using `sd_ble_gatts_characteristic_add`
+  // TODO: 6.7. Set characteristic length in number of bytes in attr_char_value structure
 
-    return NRF_SUCCESS;
+
+  // TODO: 6.4. Add new characteristic to the service using `sd_ble_gatts_characteristic_add`
+
+  return NRF_SUCCESS;
 }
