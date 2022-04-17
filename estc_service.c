aa1dd6823158f0c60890ef29d47e83371b3d1599
Vladimir Bespalov <bespalvovan42@gmail.com>
2022-04-17 14:00:20 +0300

    Copied service init function from BLE-service branch

--- estc_service.c
+++ estc_service.c
@@ -42,13 +42,15 @@ static ret_code_t estc_ble_add_characteristics(ble_estc_service_t *service);
 ret_code_t estc_ble_service_init(ble_estc_service_t *service)
 {
   ret_code_t error_code = NRF_SUCCESS;
+  ble_uuid128_t base_uuid = {ESTC_BASE_UUID};
+  ble_uuid_t ble_uuid = {.uuid = ESTC_SERVICE_UUID};
 
-  // TODO: 4. Add service UUIDs to the BLE stack table using `sd_ble_uuid_vs_add`
-  // TODO: 5. Add service to the BLE stack using `sd_ble_gatts_service_add`
+  error_code = sd_ble_uuid_vs_add(&base_uuid, &ble_uuid.type);
+  VERIFY_SUCCESS(error_code);
 
-  // NRF_LOG_DEBUG("%s:%d | Service UUID: 0x%04x", __FUNCTION__, __LINE__, service_uuid.uuid);
-  // NRF_LOG_DEBUG("%s:%d | Service UUID type: 0x%02x", __FUNCTION__, __LINE__, service_uuid.type);
-  // NRF_LOG_DEBUG("%s:%d | Service handle: 0x%04x", __FUNCTION__, __LINE__, service->service_handle);
+  ble_uuid.uuid = ESTC_SERVICE_UUID;
+
+  error_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY, &ble_uuid, &service->service_handle);
 
   return estc_ble_add_characteristics(service);
 }
